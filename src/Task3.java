e66d3d5dd4701ba5dbff1c9d51c15dcdbeffb3f6
Izotov26 <grigorizotovg@gmail.com>
2025-04-19 14:38:30 +0400

    добавили задание №3

---
+++ src/Task3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Задание 3");
+        float n = 3.56f ;
+        float m = Math.round(n);
+        System.out.println(m);
+
+    }
+}
