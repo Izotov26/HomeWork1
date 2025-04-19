bf7f5aeb99e7aa6a220d3af6ad133da8e00c8e40
Izotov26 <grigorizotovg@gmail.com>
2025-04-19 15:17:18 +0400

    добавили задание №5

---
+++ src/Task5.java
@@ -0,0 +1,9 @@
+public class Task5 {
+    public static void main(String[] args) {
+        final int R = 15;
+        double area = Math.PI *( R * R);
+        System.out.println("Площадь круга" + Math.round(area));
+        double dlina=Math.PI*2*R;
+        System.out.println("Длинна окружности"+ Math.round(dlina));
+    }
+}
